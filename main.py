68b6c303d41ba54daecee47a8534dd163ad9ce5b
Anish Raghavendra <49672329+z404@users.noreply.github.com>
2022-04-19 22:36:32 +0530

    tweet functionality created

---
+++ main.py
@@ -0,0 +1,49 @@
+from dotenv import dotenv_values
+import tweepy
+import fastapi
+from fastapi_utils.tasks import repeat_every
+import datetime
+
+config = dotenv_values()
+print(config)
+
+auth = tweepy.OAuth1UserHandler(
+   config["API_KEY"], config["API_KEY_SECRET"],
+   config["ACCESS_TOKEN"], config["ACCESS_TOKEN_SECRET"], config["BEARER_TOKEN"]
+)
+# api = tweepy.API(auth)
+client = tweepy.Client(bearer_token=config["BEARER_TOKEN"], consumer_key=config["API_KEY"], consumer_secret=config["API_KEY_SECRET"], access_token=config["ACCESS_TOKEN"], access_token_secret=config["ACCESS_TOKEN_SECRET"])
+
+app = fastapi.FastAPI()
+
+birthdays = []
+
+def post_tweet(tweet: str):
+    return client.create_tweet(text=tweet)
+
+@app.get("/add_birthday")
+def add_birthday(username: str, birthday: str):
+    """
+    Add a birthday to database (date in dd-mm-yyyy format)
+    """
+    birthdays.append({"username": username, "birthday": birthday})
+    return {"message": "Birthday added"}
+
+@app.get("/get_birthdays")
+def get_birthdays():
+    """
+    Get all birthdays from database
+    """
+    return birthdays
+
+@app.on_event("startup")
+@repeat_every(seconds=20) # 20 SECONDS USED FOR TESTING
+def tweet_birthdays():
+    """
+    Tweet all birthdays from database
+    """
+    tweetid = []
+    for birthday in birthdays:
+        if datetime.datetime.strptime(birthday["birthday"], "%d-%m-%Y").date() == datetime.datetime.now().date():
+            tweetid.append(post_tweet(f"Happy birthday @{birthday['username']}!"))
+    print("TWEETED:",tweetid)
